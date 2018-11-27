1abbbfc1785a7a02fae84a3a63d46ef68f72a849
Cleber Campomori <cleber.campomori@gmail.com>
2018-11-27 14:02:36 -0200

    Aula 5.2: Conhecendo o LinkedHashSet

--- tw-java-collections/src/br/com/treinaweb/java/collections/MainSet.java
+++ tw-java-collections/src/br/com/treinaweb/java/collections/MainSet.java
@@ -1,6 +1,7 @@
 package br.com.treinaweb.java.collections;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import br.com.treinaweb.java.collections.models.Pessoa;
@@ -8,12 +9,16 @@ import br.com.treinaweb.java.collections.models.Pessoa;
 public class MainSet {
 
 	public static void main(String[] args) {
-		Set<Pessoa> set = new HashSet<Pessoa>();
+		Set<Pessoa> set = new LinkedHashSet<Pessoa>();
 		set.add(new Pessoa(1, "TreinaWeb 1"));
 		set.add(new Pessoa(2, "TreinaWeb 2"));
-		System.out.println(set);
+		set.add(null);
+		System.out.println(set);		
 		set.add(new Pessoa(2, "TreinaWeb 2"));
 		System.out.println(set);
+		for (Pessoa p : set) {
+			System.out.println(p);
+		}
 		// ********
 		//| Chave | Valor                       |
 		//---------------------------------------
