90c3acb5f449f7e6e63befb50d76059cf94cb9c7
Cleber Campomori <cleber.campomori@gmail.com>
2018-11-27 13:27:49 -0200

    Aula 3.9: A classe abstrata AbstractSequentialList

--- tw-java-collections/src/br/com/treinaweb/java/collections/Main.java
+++ tw-java-collections/src/br/com/treinaweb/java/collections/Main.java
@@ -4,16 +4,17 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 import br.com.treinaweb.java.collections.models.Pessoa;
 
 public class Main {
 
 	public static void main(String[] args) {
-		List<Pessoa> pessoas = new LinkedList<Pessoa>();
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
 		pessoas.add(new Pessoa(1, "TreinaWeb 1"));
 		pessoas.add(new Pessoa(2, "TreinaWeb 2"));
-		pessoas.add(new Pessoa(2, "TreinaWeb 3"));	
+		pessoas.add(new Pessoa(2, "TreinaWeb 3"));
 		
 		pessoas.remove(new Pessoa(1, "TreinaWeb 1"));
 		
@@ -28,7 +29,7 @@ public class Main {
 //			System.out.println(p);
 //		}
 		
-		System.out.println("Com iterator: ");
+//		System.out.println("Com iterator: ");
 		//       |                   
 		// [p1, p2]
 //		Iterator<Pessoa> iteratorPessoa = pessoas.iterator();
@@ -40,11 +41,11 @@ public class Main {
 //			}
 //			System.out.println(p);
 //		}
-		pessoas.removeIf(p -> p.getId() == 1);
-		System.out.println(pessoas);
+//		pessoas.removeIf(p -> p.getId() == 1);
+//		System.out.println(pessoas);
 				
 		// For-each
-//		System.out.println("Com For-each:");
+//		System.out.println("For-each:");
 //		for (Pessoa p : pessoas) {
 //			System.out.println(p);
 //		}
