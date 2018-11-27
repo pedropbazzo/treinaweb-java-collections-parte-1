3bc597104201a81a21dfeae1f8b1e377d0c29f3a
Cleber Campomori <cleber.campomori@gmail.com>
2018-11-27 12:18:09 -0200

    Aula 2.5: Lidando com ArrayLists - parte 2

--- tw-java-collections/src/br/com/treinaweb/java/collections/Main.java
+++ tw-java-collections/src/br/com/treinaweb/java/collections/Main.java
@@ -1,9 +1,17 @@
 package br.com.treinaweb.java.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.treinaweb.java.collections.models.Pessoa;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		pessoas.add(new Pessoa(1, "TreinaWeb 1"));
+		pessoas.add(new Pessoa(2, "TreinaWeb 2"));
+		System.out.println(pessoas.toString());
 
 	}
 
