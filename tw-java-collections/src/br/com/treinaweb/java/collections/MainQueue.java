a9842fceedbd385b715ad4949aaae52dd3fa2ed5
Cleber Campomori <cleber.campomori@gmail.com>
2018-11-27 14:13:57 -0200

    Aula 6.2: Queues e os métodos peek() e poll()

--- tw-java-collections/src/br/com/treinaweb/java/collections/MainQueue.java
+++ tw-java-collections/src/br/com/treinaweb/java/collections/MainQueue.java
@@ -14,6 +14,7 @@ public class MainQueue {
 		queue.add(new Pessoa(3, "ATreinaWeb 3"));
 		queue.add(new Pessoa(1, "TreinaWeb 1"));
 		queue.add(new Pessoa(2, "TreinaWeb 2"));
+		queue.offer(new Pessoa(4, "TreinaWeb 4"));
 		System.out.println(queue);
 		while (!queue.isEmpty()) {
 			Pessoa p = queue.poll();
